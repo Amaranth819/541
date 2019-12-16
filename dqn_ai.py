7d8200e597add9aff700e967825eea9cd3997378
Amaranth819 <33890170+Amaranth819@users.noreply.github.com>
2019-12-16 04:55:12 -0500

    Add files via upload

--- dqn_ai.py
+++ dqn_ai.py
@@ -11,86 +11,94 @@ from dqn import DQN, resized_img, actions, weight_init
 from game import best_score
 from collections import deque
 from tensorboardX import SummaryWriter
+from torch.autograd import Variable
 
 # random.seed(1e6)
 
-# [80, 80, 3] -> [80, 80]
 def bgr2gray(rgb_img):
-    gray_img = cv2.cvtColor(cv2.resize(rgb_img, resized_img), cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.cvtColor(cv2.resize(rgb_img, resized_img), cv2.COLOR_RGB2GRAY)
     _, gray_img = cv2.threshold(gray_img, 1, 255, cv2.THRESH_BINARY)
-    gray_img = np.array(gray_img, dtype = np.float32)
+    gray_img = np.array(gray_img, dtype = np.float32) / 255
     return gray_img
 
 def data2tensor(data, device):
     return torch.tensor(data, dtype = torch.float32, device = device)
 
 # for expectimax
-def getExpectimax(ex_max,height_proba,s_mat, h_mat, a_mat, step):
+def getExpectimax(ex_max, height_proba, s_mat, h_mat, a_mat, step):
     # ex_max: expectiMax
     # # the structure of each element in ex_max: 
     # # [height, # of occurrance, # up survival, # of up not survival, # down survival, # of down not survival]
     # i is the row number
+    fix = 100
     for i in range(len(s_mat)):
-    row_len = len(s_mat[i])
-    # j is the col number
-    for j in range(row_len):
-        for k in range(len(ex_max)):
-        if ex_max[k][0] == h_mat[i][j]:
-            ex_max[k][1] += 1
-            # check whether the bird is alive after # of step
-            if j + step <= len(s_mat[i]):
-            # up action
-            if a_mat[i][j][0] == 0:
-                ex_max[k][2] += 1
-            # down action
-            else:
-                ex_max[k][4] += 1
-            else:
-            if a_mat[i][j][0] == 0:
-                ex_max[k][3] += 1
-            else:
-                ex_max[k][5] += 1
-            break
-        elif k == len(ex_max)-1:
-            # print("Current height: ")
-            # print(h_mat[i][j])
-            ex_max = np.append(ex_max,[[h_mat[i][j],0,0,0,0,0]],axis = 0)
-            ex_max[-1][1] += 1
-            if j + 10 <= len(s_mat[i]):
-            if a_mat[i][j][0] == 0:
-                ex_max[-1][2] += 1
-            else:
-                ex_max[-1][4] += 1
-            else:
-            if a_mat[i][j][0] == 0:
-                ex_max[-1][3] += 1
-            else:
-                ex_max[-1][5] += 1
-    ex_max = np.delete(ex_max,0,0)
+        row_len = len(s_mat[i])
+        # j is the col number
+        row_lens = random.sample(range(row_len), fix) if row_len > fix else range(row_len)
+        for j in row_lens:
+            for k in range(len(ex_max)):
+                if ex_max[k][0] == h_mat[i][j]:
+                    ex_max[k][1] += 1
+                    # check whether the bird is alive after # of step
+                    if j + step < len(s_mat[i]):
+                        # up action
+                        if a_mat[i][j][0] == 0:
+                            ex_max[k][2] += 1
+                        # down action
+                        else:
+                            ex_max[k][4] += 1
+                    else:
+                        if a_mat[i][j][0] == 0:
+                            ex_max[k][3] += 1
+                        else:
+                            ex_max[k][5] += 1
+                    break
+                elif k == len(ex_max)-1:
+                    # print("Current height: ")
+                    # print(h_mat[i][j])
+                    ex_max = np.append(ex_max,[[h_mat[i][j],10,5,0,5,0]],axis = 0)
+                    ex_max[-1][1] += 1
+                    if j + 10 <= len(s_mat[i]):
+                        if a_mat[i][j][0] == 0:
+                            ex_max[-1][2] += 1
+                        else:
+                            ex_max[-1][4] += 1
+                    else:
+                        if a_mat[i][j][0] == 0:
+                            ex_max[-1][3] += 1
+                        else:
+                            ex_max[-1][5] += 1
+    # ex_max = np.delete(ex_max,0,0)
     # print(ex_max)
     # calculate the survival prob at specific height for doing different action
-    for row in ex_max:
-    if row[1] != 0:
-        height_proba = np.append(height_proba,[[row[0],row[2]/row[1],row[4]/row[1]]],axis = 0)
-    else:
-        height_proba = np.append(height_proba,[[row[0],0,0]],axis = 0)
-    height_proba = np.delete(height_proba,0,0)
+
+    # for row in ex_max:
+    #     if row[1] != 0:
+    #         height_proba = np.append(height_proba,[[row[0],row[2]/row[1],row[4]/row[1]]],axis = 0)
+    #     else:
+    #         height_proba = np.append(height_proba,[[row[0],0,0]],axis = 0)
+    
+    # height_proba = np.delete(height_proba,0,0)
     # print(height_proba)
-    return ex_max,height_proba
+    return ex_max, height_proba
   
            
-def getHeightDecision(height_proba,height):
+def getHeightDecision(ex_max, height):
     # print(height_proba)
-    for row in height_proba:
+    result = np.array([1, 0], dtype = np.float32)
+    for row in ex_max:
+        up_survival = row[2] / row[1]
+        up_dead = row[3] / row[1]
+        down_survival = row[4] / row[1]
+        down_dead = row[5] / row[1]
         if row[0] == height:
-            if row[1] > row[2]:
+            if up_survival >= down_survival and up_dead <= down_dead:
                 result = np.array([0,1], dtype = np.float32)
             else:
                 result = np.array([1,0], dtype = np.float32)
-        return result
-
+            break
     # By default
-    return np.array([1, 0], dtype = np.float32)
+    return result
 
 
 def start(mode = 'train'):
@@ -100,7 +108,7 @@ def start(mode = 'train'):
     '''
         Configuration
     '''
-    observe_steps = 20
+    observe_steps = 5
     memory_size = 1000
     epoch = 5000 # Game 
     use_pretrained_model = False
@@ -112,11 +120,14 @@ def start(mode = 'train'):
     final_epsilon = 0.0001
     frame_per_action = 1
     epsilon = init_epsilon if mode is 'train' else 0
-    init_learning_rate = 1e-2
-    batch_size = 16
+    init_learning_rate = 1e-6
+    batch_size = 2
     start_epoch = 1
     gamma = 0.99
     history_size = 4
+    max_window_size = 128
+    random.seed(1e6)
+    use_expectimax = True
 
     # for expectiMax 
     # ex_max: detail data of expectiMax
@@ -124,7 +135,7 @@ def start(mode = 'train'):
     # height_proba: the probability of up action and down action.
     height_proba = np.zeros((1,3))
     # 
-    step = 10
+    step = 60
 
     if os.path.exists(save_model_path) is False:
         os.makedirs(save_model_path)
@@ -138,7 +149,8 @@ def start(mode = 'train'):
     net = DQN().to(device).float()
     loss_func = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(net.parameters(), lr = init_learning_rate, momentum = 0.9)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 10000, gamma = 0.1)
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 1000, gamma = 0.1)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones = [500, 2000], gamma = 0.5)
     # Read the pretrained model
     if use_pretrained_model:
         checkpoint = torch.load(pretrained_model_path)
@@ -170,7 +182,7 @@ def start(mode = 'train'):
     img, reward, terminate = flappybird.frame_step(aidx)
     curr_height = flappybird.playery
     img = bgr2gray(img)
-    # img_seq: 4x80x80
+    # img_seq: 4x84x84
     img_seq = np.stack([img for _ in range(history_size)], axis = 0)
 
     '''
@@ -178,26 +190,30 @@ def start(mode = 'train'):
     '''
     # Train DQN
     if mode is 'train':
+        net.train()
         stage = 'OBSERVE'
         print('Start training...')
         for e in range(start_epoch, epoch + start_epoch):
             per_game_memory = deque()
             while True:
                 # img_seq_ts = data2tensor(img_seq, device).unsqueeze(0)
-                img_seq_ts = torch.from_numpy(img_seq).unsqueeze(0).to(device)
+                img_seq_ts = Variable(torch.from_numpy(img_seq).unsqueeze(0).to(device))
                 pred = net(img_seq_ts)
 
                 # Take an action
                 idx, action = 0, np.zeros([actions], dtype = np.float32)
                 if e % frame_per_action == 0:
-                    # Epsilon greedy policy
-                    if random.random() <= epsilon:
-                        idx = random.randint(0,1)
+                    if stage is 'OBSERVE':
+                        idx = 0 if random.random() < 0.9 else 1
                     else:
-                        idx = torch.argmax(pred, dim = 1).item()
+                        # Epsilon greedy policy
+                        if random.random() <= epsilon:
+                            idx = 0 if random.random() < 0.9 else 1
+                        else:
+                            idx = torch.argmax(pred, dim = 1).item()
                 else:
                     idx = 0
-                action[random.randint(0,1)] = 1
+                action[idx] = 1
 
                 # Scale down epsilon
                 epsilon -= (init_epsilon - final_epsilon) / epoch
@@ -221,42 +237,52 @@ def start(mode = 'train'):
                 # Train after observation
                 if e > start_epoch + observe_steps:
                     stage = 'TRAINING'
+                    # Get all history of 'batch_size' games
                     batch = random.sample(memory_replay, batch_size)
-                    # img_seq_b, img_seq_next_b, action_b, reward_b, terminate_b, curr_height_b = zip(*batch)
-                    img_seq_b = [b[0] for b in batch]
-                    img_seq_next_b = [b[1] for b in batch]
-                    action_b = [b[2] for b in batch]
-                    reward_b = [b[3] for b in batch]
-                    terminate_b = [b[4] for b in batch]
-                    curr_height_b = [b[5] for b in batch]
 
-                    for i in range(len(action_b)):
-                        s_mat = terminate_b[i][:-1]
-                        h_mat = curr_height_b[i][:-1]
-                        a_mat = action_b[i][:-1]
-                        ex_max, height_proba = getExpectimax(ex_max, height_proba, s_mat, 
-                                                                h_mat, a_mat, step)
+                    # Get the game history
+                    img_seq_b, img_seq_next_b, action_b, reward_b, terminate_b, curr_height_b = [], [], [], [], [], []
+                    for b in batch:
+                        ib, inb, ab, rb, tb, chb = zip(*b)
+                        img_seq_b.append(ib)
+                        img_seq_next_b.append(inb)
+                        action_b.append(ab)
+                        reward_b.append(rb)
+                        terminate_b.append(tb)
+                        curr_height_b.append(chb)
+                    reward_b_window = np.concatenate(reward_b, axis = 0)
+                    terminate_b_window = np.concatenate(terminate_b, axis = 0)
 
+                    # Randomly sample if there are too many frames in total.
+                    all_frames_num = terminate_b_window.shape[0]
+                    window = np.array(random.sample(list(range(all_frames_num)), max_window_size)) if all_frames_num > max_window_size else np.array(list(range(all_frames_num)))
+                    reward_b_window = np.take(reward_b_window, window, axis = 0)
+                    terminate_b_window = np.take(terminate_b_window, window, axis = 0)
 
-                    # img_seq_b_ts = data2tensor(np.stack([ib for ib in img_seq_b], axis = 0), device)
-                    img_seq_b_ts = torch.from_numpy(np.stack([ib for ib in img_seq_b], axis = 0)).to(device)
-                    # img_seq_next_b_ts = data2tensor(np.stack([ib for ib in img_seq_next_b], axis = 0), device)
-                    img_seq_next_b_ts = torch.from_numpy(np.stack([ib for ib in img_seq_next_b], axis = 0)).to(device)
-                    # action_b_ts = data2tensor(action_b, device)
-                    # action_b_ts = torch.from_numpy(np.array(list(action_b))).to(device)
+                    # Get the image states
+                    img_seq_b_ts = Variable(torch.from_numpy(np.take(np.concatenate(img_seq_b, axis = 0), window, axis = 0)).to(device))
+                    img_seq_next_b_ts = Variable(torch.from_numpy(np.take(np.concatenate(img_seq_next_b, axis = 0), window, axis = 0)).to(device))
 
-                    # get the action according to the current height and expectimax
-                    action_b_ts = torch.from_numpy(getHeightDecision(height_proba, curr_height)).to(device)
+                    if use_expectimax is True:
+                        s_mat = terminate_b[:][:-1]
+                        h_mat = curr_height_b[:][:-1]
+                        a_mat = action_b[:][:-1]
+                        ex_max, height_proba = getExpectimax(ex_max, height_proba, s_mat, h_mat, a_mat, step)
+                        action_b_ts = torch.from_numpy(getHeightDecision(ex_max, curr_height)).to(device)
+                    else:
+                        action_b_window = np.take(np.concatenate(action_b, axis = 0), window, axis = 0)
+                        action_b_ts = torch.from_numpy(np.array(action_b_window)).to(device)
+
+                    # Predict
                     out = net(img_seq_b_ts)
                     out_next = net(img_seq_next_b_ts)
-                    # y_b = torch.tensor([reward_b[bi] if terminate_b[bi] else reward_b[bi] + gamma * torch.max(out_next).item() for bi in range(batch_size)]).to(device)
+                    # Calculate y and q value
                     y_b = []
-                    for r, t, p in zip(reward_b, terminate_b, out_next):
+                    for r, t, p in zip(reward_b_window, terminate_b_window, out_next):
                         if t:
                             y_b.append(r)
                         else:
                             y_b.append(r + gamma * torch.max(p).item())
-                    # y_b = data2tensor(y_b, device)
                     y_b = torch.from_numpy(np.array(y_b, dtype = np.float32)).to(device)
                     q_value_b = torch.sum(out * action_b_ts, dim = 1)
 
@@ -275,22 +301,23 @@ def start(mode = 'train'):
 
                 img_seq = img_seq_next
 
-                # Save model
-                if e % 10000 == 0:
-                    states = {
-                        'epoch' : e,
-                        'state_dict' : net.state_dict(),
-                        'optimizer' : optimizer.state_dict(),
-                        'scheduler' : scheduler.state_dict()
-                    }
-                    torch.save(states, save_model_path + save_model_name)
-                    print('Save the model at epoch %d successfully!' % e)
-
                 scheduler.step()   
 
                 if terminate:
                     break    
 
+            # Save model
+            if e % 50 == 0:
+                states = {
+                    'epoch' : e,
+                    'state_dict' : net.state_dict(),
+                    'optimizer' : optimizer.state_dict(),
+                    'scheduler' : scheduler.state_dict()
+                }
+                torch.save(states, save_model_path + save_model_name)
+                print('Save the model at epoch %d successfully!' % e)
+
+            # Update the memory
             memory_replay.append(per_game_memory)
             if len(memory_replay) > memory_size:
                 memory_replay.popleft()     
--- model/dqn2.py
+++ model/dqn2.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import random
 
-resized_img = (84, 84)
+resized_img = (80, 80)
 actions = 2 # action[0] = 1 means no flapping and action[1] = 1 means flapping.
 
-# [None, 4, 84, 84] -> [None, 2]
+# [None, 4, 80, 80] -> [None, 2]
 
 class DQN(nn.Module):
     '''
@@ -19,30 +19,20 @@ class DQN(nn.Module):
             Build the network
         '''
         # Encoder
-        # bs*4*84*84 -> bs*32*20*20
+        # bs*4*80*80 -> bs*32*20*20
         self.conv1 = nn.Sequential(
-            nn.Conv2d(4, 32, kernel_size = 8, stride = 4),
+            nn.Conv2d(4, 32, kernel_size = 4, stride = 4, padding = 0),
             nn.ReLU(inplace = True)
         )
-
-        # # bs*32*20*20 -> bs*32*10*10
-        # self.down_sampling1 = nn.MaxPool2d(2, 2)
-
-        # bs*32*20*20 -> bs*64*9*9
+        # bs*32*20*20 -> bs*64*10*10
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size = 4, stride = 2),
-            nn.ReLU(inplace = True)
-        )
-
-        # bs*64*9*9 -> bs*64*7*7
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size = 3, stride = 1),
+            nn.Conv2d(32, 64, kernel_size = 3, stride = 2, padding = 1),
             nn.ReLU(inplace = True)
         )
 
         # FC
         self.fc_relu = nn.Sequential(
-            nn.Linear(64*7*7, 512),
+            nn.Linear(64*10*10, 512),
             nn.ReLU(inplace = True),
             nn.Linear(512, actions)
         )
@@ -58,9 +48,8 @@ class DQN(nn.Module):
     def forward(self, x):
         # Encoder
         x = self.conv1(x)
-        # x = self.down_sampling1(x)
         x = self.conv2(x)
-        x = self.conv3(x)
+        # x = self.conv3(x)
 
         # GRU
         # hidden = self.init_hidden()
@@ -74,22 +63,14 @@ class DQN(nn.Module):
 
         return x
 
-# def weight_init(net):
-#     for each_module in net.modules():
-#         if isinstance(each_module, nn.Conv2d):
-#             torch.nn.init.xavier_uniform_(each_module.weight)
-#         elif isinstance(each_module, nn.Linear):
-#             each_module.weight.data.normal_(-1, 1)
-#             if each_module.bias is not None:
-#                 each_module.bias.data.normal_(-1, 1)
-
-
 def weight_init(net):
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-            torch.nn.init.uniform_(m.weight, -0.1, 0.1)
-            torch.nn.init.constant_(m.bias, 0)
-
+    for each_module in net.modules():
+        if isinstance(each_module, nn.Conv2d):
+            torch.nn.init.xavier_uniform_(each_module.weight)
+        elif isinstance(each_module, nn.Linear):
+            each_module.weight.data.normal_(-0.1, 0.1)
+            if each_module.bias is not None:
+                each_module.bias.data.zero_()
 
 def randomly_get_action():
     idx = 0 if random.random() < 0.8 else 1
